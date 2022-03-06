0b93a19889cc9c618776418b6f02d7e73afcf341
=ARA <g12.Khachaturov.ara@gmail.com>
2022-03-06 18:19:12 +0400

    almost Done

--- index.js
+++ index.js
@@ -1,66 +1,31 @@
 let moment=require("moment");
-console.log("hi");
-let k = "hellow";
-console.log(k);
-console.log(moment().format('yyyy/MM/DD'));
+let i=0;
+
 
 const stream =require('stream');
-const { Writable } = require('stream');
-
-const myWritable = new Writable({
-  write(chunk, encoding, callback) {
-      console.log(chunk.toString());
-    if (chunk.toString().indexOf('a') >= 0) {
-      callback(new Error('chunk is invalid'));
-    } else {
-      callback();
-    }
-  }
-});
-
-
-myWritable.write('a');
-
-
 const { Readable } = require('stream');
+const {Transform} =require("stream");
 
-const myReadable = new Readable({
-  read(size) {
-    const err = checkSomeErrorCondition();
-    if (err) {
-      this.destroy(err);
-    } else {
-      // Do some work.
+const myReadableStream=new Readable({
+    read(size){
+        setTimeout(() => {
+            this.push('now');
+        }, 1000);
+        
     }
-  }
 });
 
-/*
-const { Transform } = require('stream');
+const myTransform=new Transform({
+transform(chunk,encoding,callback){
+    let formatedDate=moment(chunk.toString()).format('yyyy-MM-DD');
+    console.log(formatedDate);
+}
 
-// All Transform streams are also Duplex Streams.
-const myTransform = new Transform({
-  writableObjectMode: true,
-
-  transform(chunk, encoding, callback) {
-    // Coerce the chunk to a number if necessary.
-    chunk |= 0;
-
-    // Transform the chunk into something else.
-    const data = chunk.toString(16);
-
-    // Push the data onto the readable queue.
-    callback(null, '0'.repeat(data.length % 2) + data);
-  }
 });
 
-myTransform.setEncoding('ascii');
-myTransform.on('data', (chunk) => console.log(chunk));
+let date=new Date();
+console.log(moment(date).format('yyyy/MM/DD'));
 
-myTransform.write(1);
-// Prints: 01
-myTransform.write(10);
-// Prints: 0a
-myTransform.write(100);
-// Prints: 64
+myTransform.write(date.toISOString());
+
 
