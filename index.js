bc4a2e4891da9abd0e4f42582d11ad0d9a395f5c
=ARA <g12.Khachaturov.ara@gmail.com>
2022-03-06 17:31:07 +0400

    all streams are created

--- index.js
+++ index.js
@@ -3,3 +3,64 @@ console.log("hi");
 let k = "hellow";
 console.log(k);
 console.log(moment().format('yyyy/MM/DD'));
+
+const stream =require('stream');
+const { Writable } = require('stream');
+
+const myWritable = new Writable({
+  write(chunk, encoding, callback) {
+      console.log(chunk.toString());
+    if (chunk.toString().indexOf('a') >= 0) {
+      callback(new Error('chunk is invalid'));
+    } else {
+      callback();
+    }
+  }
+});
+
+
+myWritable.write('a');
+
+
+const { Readable } = require('stream');
+
+const myReadable = new Readable({
+  read(size) {
+    const err = checkSomeErrorCondition();
+    if (err) {
+      this.destroy(err);
+    } else {
+      // Do some work.
+    }
+  }
+});
+
+/*
+const { Transform } = require('stream');
+
+// All Transform streams are also Duplex Streams.
+const myTransform = new Transform({
+  writableObjectMode: true,
+
+  transform(chunk, encoding, callback) {
+    // Coerce the chunk to a number if necessary.
+    chunk |= 0;
+
+    // Transform the chunk into something else.
+    const data = chunk.toString(16);
+
+    // Push the data onto the readable queue.
+    callback(null, '0'.repeat(data.length % 2) + data);
+  }
+});
+
+myTransform.setEncoding('ascii');
+myTransform.on('data', (chunk) => console.log(chunk));
+
+myTransform.write(1);
+// Prints: 01
+myTransform.write(10);
+// Prints: 0a
+myTransform.write(100);
+// Prints: 64
+
