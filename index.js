5782b880edc30587c6a675c45e5121baa0e4f897
=ARA <g12.Khachaturov.ara@gmail.com>
2022-03-08 19:20:31 +0400

    ready to use

--- index.js
+++ index.js
@@ -2,36 +2,30 @@ let fs = require("fs");
 let stream = require("stream");
 let { Readable } = stream;
 let { Writable } = stream;
-let {Transform} = stream;
-let moment=require('moment');
-let filPah='text.txt';
-let i =0;
-let readableStream= new Readable({
-    read(size){
-        
-  
-                let currentDateTime=new Date(); 
-                this.push(currentDateTime.toISOString());               
-    }
-});
-let transform= new Transform({
-    transform(chunk,encoding,callback){
-        //console.log(chunk.toString());
-        this.push(moment(chunk.toString()).format('yyyy-MM-DD HH:mm:ss')+'\n');
-        callback();
-    }
-});
-let writableStream= new Writable({
-    write(chunk,encoding,callback){
+let { Transform } = stream;
+let moment = require("moment");
+let filPah = __dirname+"text.txt";
 
-            fs.writeFile(filPah,chunk,{flag:'a'},callback);
- 
-        
-    }
+let readableStream = new Readable({
+  read(size) {
+    let action = setInterval(() => {
+      clearInterval(action);
+      let currentDateTime = new Date();
+      this.push(currentDateTime.toISOString());
+    }, 1000);
+  },
 });
-
-readableStream.on('data',chunk=>{
+let transform = new Transform({
+  transform(chunk, encoding, callback) {
     console.log(chunk.toString());
-})
+    this.push(moment(chunk.toString()).format("yyyy-MM-DD HH:mm:ss") + "\n");
+    callback();
+  },
+});
+let writableStream = new Writable({
+  write(chunk, encoding, callback) {
+    fs.writeFile(filPah, chunk, { flag: "a" }, callback);
+  },
+});
 
-
+readableStream.pipe(transform).pipe(writableStream);
